c6073c1325b999eb5f08ee21285c76ce3f081699
shogo07xx <mori.shogo_am@icloud.com>
2023-07-02 21:44:22 +0900

    style: left-justify bulleted items

--- routine/models.py
+++ routine/models.py
@@ -12,9 +12,9 @@ class NoSQLBase(models.Model):
     @classmethod
     def get_dynamodb_table(self):
         session = boto3.Session(
-            aws_access_key_id='AKIA2HQ6AQ7J2RVE4OB3',
-            aws_secret_access_key='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',
-            region_name='ap-northeast-3'
+            aws_access_key_id = 'AKIA2HQ6AQ7J2RVE4OB3',
+            aws_secret_access_key = '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',
+            region_name = 'ap-northeast-3'
         )
         dynamodb = session.resource('dynamodb')
         return dynamodb.Table(self.table_name)
@@ -68,14 +68,14 @@ class Interest(models.Model): # 外部キーのため依存解消のために仮
 
 class Routine(models.Model):
     table_name   = 'routine'
-    user_id      = models.ForeignKey(User, on_delete=models.CASCADE) # user_idはバックエンドで取得します。現段階では仮の数字を代入してください
-    interest_id  = models.OneToOneField(Interest, on_delete=models.PROTECT)# interest_idはバックエンドで取得します。現段階では仮の数字を代入してください
-    goal_id      = models.IntegerField()# interest_idはバックエンドで取得します。現段階では仮の数字を代入してください
-    dow          = models.IntegerField()  # 型は仮置き # day_of_week(曜日のこと)
+    user_id      = models.ForeignKey(User, on_delete=models.CASCADE)        # user_id はバックエンドで取得      # 仮の数字を代入して対処
+    interest_id  = models.OneToOneField(Interest, on_delete=models.PROTECT) # interest_id はバックエンドで取得  # 仮の数字を代入して対処
+    goal_id      = models.IntegerField()                                    # interest_id はバックエンドで取得  # 仮の数字を代入して対処
+    dow          = models.IntegerField()                                    # 型は仮置き  # day_of_week (曜日のこと)
     start_time   = models.TimeField()
-    end_time     = models.TimeField()  # 時間未設定タスクを含んだ幅を持たせる
-    title        = models.CharField(max_length=15)  # 10 文字に余裕を持たせて 15 文字
-    subtitle     = models.CharField(max_length=40, blank=True)  # 簡易的な補足説明
+    end_time     = models.TimeField()                                       # 時間未設定タスクを含んだ幅を持たせる
+    title        = models.CharField(max_length=15)                          # 10 文字に余裕を持たせて 15 文字
+    subtitle     = models.CharField(max_length=40, blank=True)              # 簡易的な補足説明
     icon         = models.CharField(max_length=1, blank=True)
     public       = models.BooleanField(help_text='公開RoutineならTrue', default=False)
     notification = models.BooleanField(help_text='通知onならTrue', default=False)
@@ -86,8 +86,8 @@ class Routine(models.Model):
 class Task(models.Model):
     table_name    = 'task'
     routine_id    = models.ForeignKey(Routine, on_delete=models.CASCADE)
-    title         = models.CharField(max_length=20)  # ルーティンタイトルの限度より少し長い程度
-    detail        = models.CharField(max_length=60, blank=True)  # 60 文字に仮置き. あまり情報を詰め込みすぎないことを目標に.
+    title         = models.CharField(max_length=20)             # ルーティンタイトルの限度より少し長い程度
+    detail        = models.CharField(max_length=60, blank=True) # 60 文字に仮置き  # あまり情報を詰め込みすぎないことが目標
     icon          = models.CharField(max_length=1, blank=True)
     required_time = models.IntegerField()
     notification  = models.BooleanField(help_text='通知onならTrue', default=False)
