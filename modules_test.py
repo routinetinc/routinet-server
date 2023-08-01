0dae7403c3a816a3b4a1cdc2616ec3c1b6ced116
yamadamanato <131785711+yamadamanato@users.noreply.github.com>
2023-08-01 14:58:16 +0900

    fix: change name of files

---
+++ modules_test.py
@@ -0,0 +1,51 @@
+# django.setup() 依存先環境変数値の設定. 
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'KGAvengers.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'KGAvengers.settings')
+
+# Django アプリケーションを初期化しロード
+import django
+django.setup()
+ 
+
+#*----------------------- (以下の部分にテストを記述) ---------------------------------*#
+
+from routine.fields import CustomModels
+from datetime import datetime
+import pytz
+from django.utils import timezone
+from django.utils.timezone import localtime
+RED = '\033[91m'
+END = '\033[0m'
+jst_tz = pytz.timezone('Asia/Tokyo')
+
+
+if __name__ == '__main__':
+    # routine.fields.TimeField に関するテスト
+    try:
+        time_f: CustomModels.TimeStringField = CustomModels.TimeStringField()
+        # hhmmss+tz の形式で現在時刻が表示されるかを timezone 型経由でテスト
+        print(time_f.from_db_value(localtime(timezone.now())))
+        # datetime 型経由で from_db_value, to_python, get_prep_value をテスト
+        print(time_f.from_db_value(datetime.now(jst_tz)))
+        print(time_f.to_python(datetime.now(jst_tz)))
+        print(time_f.get_prep_value(datetime.now(jst_tz)))
+    except Exception as e:
+        print(f'{RED}{e}{END}; generated during routin.fields.TimeField test')
+    finally:
+        print()
+    # routine.fields.DOWField に関するテスト
+    try:
+        dow_f: CustomModels.DOWField = CustomModels.DOWField()
+        # 引数 b1000101 -> ['0', '2', '6'] となるかをテスト
+        print(dow_f.dow_from_int_to_list(int('1000101', 2)))
+        # 引数 ['0', '2', '6'] -> b1000101 となるかをテスト
+        print(f"b{dow_f.dow_from_list_to_int(['0', '2', '6']):07b}")
+        # from_db_value, to_python, get_prep_value をテスト
+        print(dow_f.from_db_value(int('1000101', 2)))
+        print(dow_f.from_db_value(int('1000101', 2)))
+        print(f"b{dow_f.get_prep_value(['0', '2', '6']):07b}")
+    except Exception as e:
+        print(f'{RED}{e}{END}; generated during routin.fields.DOWField test')  
+    finally:
+        print()
---
+++ requests_test.py
@@ -0,0 +1,48 @@
+import requests
+import json
+BLUE = '\033[36m'
+END = '\033[0m'
+
+# テスト方法: url, json_data を適した値にし、app.py を実行中に新たなターミナル (別のデスクトップを立ち上げても良い) で requests_test.py を実行.
+# requests.exceptions.JSONDecodeError: Expecting value: line 1 column 1 (char 0) の エラーは json 由来ではないため、アプリ側のエラーを参照.
+
+
+# リクエスト先 URL
+url = 'http://127.0.0.1:8000/routine/routine/'
+request_methods = {0: 'get', 1: 'post', 2: 'patch', 3: 'delete'} 
+request_method  = request_methods[1]
+
+# JSON 形式のデータ(リクエスト用). 変数 json_data の格納値にドキュメントの {'data': data_value } 全体をコピペする.
+json_data = {
+	'data': {
+		'dow': ['0', '1', '4'],  # // 月曜を　'0'　とし連番で定義。
+		'start_time': '090000+0900',  # // HHMMSSTZ の形式
+		'end_time': '090000+0900',
+		'title': 'foo', 
+		'subtitle': 'bar',  
+		'icon': '👍',
+		'is_published': True,
+		'is_notified': True
+	}
+}
+
+
+# アクセス
+headers = {'Content-Type': 'application/json'}
+if(request_method == request_methods[0]):
+  	response = requests.get(url)
+elif(request_method == request_methods[1]):
+  	response = requests.post(url, json=json_data, headers=headers)
+elif(request_method == request_methods[2]):
+  	response = requests.patch(url, json=json_data, headers=headers)
+elif(request_method == request_methods[3]):
+		response = requests.delete(url)
+else:
+  	pass
+
+print(f'{BLUE}url      ={END} {url}')
+print(f'{BLUE}headers  ={END} {response.request.headers}')
+print(f'{BLUE}body     ={END} {response.request.body}')
+print(f'{BLUE}method   ={END} {response.request.method}')
+res_data = response.json()
+print(f'{BLUE}responce ={END} {res_data}')
