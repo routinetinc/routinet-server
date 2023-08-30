3a308f85572598f336c8a54f8b915e1f149e79cf
hukurou <junjun.0712.on@gmail.com>
2023-08-30 14:30:50 +0900

    特定のエラーを特定していない例外処理を除去

--- routine/views_package/routine.py
+++ routine/views_package/routine.py
@@ -10,39 +10,32 @@ class Routine(APIView):
         pass
     
     def post(self, request, format=None):
-        try:
-            user_id = 1 if(request.user.id is None) else request.user.id
-            user = UserModel.objects.get(id=user_id)
-        except Exception as e:
-            return make_response(550, data={'error': str(e)})
+        user_id = 1 if(request.user.id is None) else request.user.id
+        user = UserModel.objects.get(id=user_id)
         try:
             datas: dict = get_json(request, serializers.Routine_create)
         except RequestInvalid as e:
-            return make_response(550, data={'error': str(e)})
             return make_response(status_code=400)
-        try: 
-            r = models.Routine(
-                    user_id = user,
-                    dow = datas['dow'],
-                    start_time = datas['start_time'],
-                    end_time = datas['end_time'],
-                    title = datas['title'],
-                    subtitle = datas['subtitle'],
-                    is_published = datas['is_published'],
-                    is_notified = datas['is_notified'],
-                    icon = datas['icon']
-                )
-            r.save()
-            datas = {'routine_id': r.id}
-        except Exception as e:
-            return make_response(550, data={'error': str(e)})
+        r = models.Routine(
+                user_id = user,
+                dow = datas['dow'],
+                start_time = datas['start_time'],
+                end_time = datas['end_time'],
+                title = datas['title'],
+                subtitle = datas['subtitle'],
+                is_published = datas['is_published'],
+                is_notified = datas['is_notified'],
+                icon = datas['icon']
+            )
+        r.save()
+        datas = {'routine_id': r.id}
         return make_response(data = datas)
     
     def patch(self, request, format=None):
         try:
             datas: dict = get_json(request, serializers.Routine_update)
         except RequestInvalid as e:
-            return make_response(550, data={'error': str(e)})
+            return make_response(status_code=400)
         try: 
             # データベースから Routine オブジェクトを取得
             r = models.Routine.objects.get(id=datas['routine_id'])
@@ -58,9 +51,6 @@ class Routine(APIView):
             r.save()
         except models.Routine.DoesNotExist:
             return make_response(status_code=404, data={'message': 'Routine not found'})
-        except:
-            return make_response(550, data={'error': str(e)})
-            return make_response(status_code=400)
         datas = {'routine_id': r.id}
         return make_response(data = datas)
     
@@ -68,94 +58,87 @@ class Routine(APIView):
         try:
             datas = get_json(request, serializers.Routine_delete)
         except RequestInvalid as e:
-            return make_response(550, data={'error': str(e)})
             return make_response(status_code=400)
         try:
             r = models.Routine.objects.get(id=datas['routine_id'])
             r.delete()
         except models.Routine.DoesNotExist:
             return make_response(status_code=404, data={'message': 'Routine not found'})
-        except:
-            return make_response(550, data={'error': str(e)})
-            return make_response(status_code=400)
         return make_response(status_code=200, data={'message': 'Routine deleted successfully'})
 
 
 #ログインユーザーの、一週間分のルーティーンとタスクを取得する。
 class ReadRoutineAndTask(APIView):                 
     def get(self, request, format=None):
-        try:
-            user_id = 1 if(request.user.id is None) else request.user.id
-            user = UserModel.objects.get(id=user_id)
+        user_id = 1 if(request.user.id is None) else request.user.id
+        user = UserModel.objects.get(id=user_id)
 
-            #dowのリストを、[0, 1, 2, 3, 4, 5, 6]の形式で、その日から順番に用意
-            now = datetime.now()
-            dow_list = [(now + timedelta(days=i)).weekday() for i in range(7)]
+        #dowのリストを、[0, 1, 2, 3, 4, 5, 6]の形式で、その日から順番に用意
+        now = datetime.now()
+        dow_list = [(now + timedelta(days=i)).weekday() for i in range(7)]
 
-            routines_data = {}
-            routines_by_day = {
-                "mon": [],
-                "tue": [],
-                "wed": [],
-                "thu": [],
-                "fri": [],
-                "sat": [],
-                "sun": []
-            }
-            day_of_week = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+        routines_data = {}
+        routines_by_day = {
+            "mon": [],
+            "tue": [],
+            "wed": [],
+            "thu": [],
+            "fri": [],
+            "sat": [],
+            "sun": []
+        }
+        day_of_week = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
 
-            for dow in dow_list:
-                #SQL文の実行結果をDjangoのインスタンスとして返す
-                routines = models.Routine.objects.raw(
-                
-                    #models.Routine.objects.raw(): Djangoで生のSQLクエリを実行。
-                    # SELECT * FROM routine_routine: ルーティンテーブルから全カラムを取得。
-                    # WHERE user_id_id = %s: ユーザーIDに基づくレコードのフィルタリング。
-                    # AND (dow & %s) != 0: 曜日情報（dow）をビット単位で解析し、該当するレコードをフィルタリング。
-                    # ORDER BY start_time: 結果をルーティンの開始時間順にソート。
+        for dow in dow_list:
+            #SQL文の実行結果をDjangoのインスタンスとして返す
+            routines = models.Routine.objects.raw(
+            
+                #models.Routine.objects.raw(): Djangoで生のSQLクエリを実行。
+                # SELECT * FROM routine_routine: ルーティンテーブルから全カラムを取得。
+                # WHERE user_id_id = %s: ユーザーIDに基づくレコードのフィルタリング。
+                # AND (dow & %s) != 0: 曜日情報（dow）をビット単位で解析し、該当するレコードをフィルタリング。
+                # ORDER BY start_time: 結果をルーティンの開始時間順にソート。
 
-                    "SELECT * FROM routine_routine WHERE user_id_id = %s AND (dow & %s) != 0 ORDER BY start_time",
+                "SELECT * FROM routine_routine WHERE user_id_id = %s AND (dow & %s) != 0 ORDER BY start_time",
 
-                    # [user.id, 2**dow]: フィルタリングと解析のためのパラメータを渡す。
-                    [user.id, 2**dow])
+                # [user.id, 2**dow]: フィルタリングと解析のためのパラメータを渡す。
+                [user.id, 2**dow])
 
-                for routine in routines:
-                    if str(routine.id) not in routines_data:
-                        tasks = models.Task.objects.filter(routine_id=routine).order_by('id')
-                        task_data = []
-                        for task in tasks:
-                            latest_task_record = models.TaskRecord.objects.filter(task_id=task.id).order_by('-when').first()
-                            is_achieved = latest_task_record.is_achieved if latest_task_record else False
-                            task_data.append({
-                                "task_id": task.id,
-                                "title": task.title,
-                                "detail": task.detail,
-                                "required_time": task.required_time,
-                                "is_notified": task.is_notified,
-                                "is_achieved": is_achieved,
-                            })
-                        routines_data[str(routine.id)] = {
-                            "start_time": routine.start_time,
-                            "end_time": routine.end_time,
-                            "title": routine.title,
-                            "subtitle": routine.subtitle,
-                            "is_published": routine.is_published,
-                            "is_notified": routine.is_notified,
-                            "tasks": task_data
-                        }
-                    routines_by_day[day_of_week[dow]].append(str(routine.id))
+            for routine in routines:
+                if str(routine.id) not in routines_data:
+                    tasks = models.Task.objects.filter(routine_id=routine).order_by('id')
+                    task_data = []
+                    for task in tasks:
+                        latest_task_record = models.TaskRecord.objects.filter(task_id=task.id).order_by('-when').first()
+                        is_achieved = latest_task_record.is_achieved if latest_task_record else False
+                        task_data.append({
+                            "task_id": task.id,
+                            "title": task.title,
+                            "detail": task.detail,
+                            "required_time": task.required_time,
+                            "is_notified": task.is_notified,
+                            "is_achieved": is_achieved,
+                        })
+                    routines_data[str(routine.id)] = {
+                        "start_time": routine.start_time,
+                        "end_time": routine.end_time,
+                        "title": routine.title,
+                        "subtitle": routine.subtitle,
+                        "is_published": routine.is_published,
+                        "is_notified": routine.is_notified,
+                        "tasks": task_data
+                    }
+                routines_by_day[day_of_week[dow]].append(str(routine.id))
 
 
-            data = {
-                "mon": routines_by_day["mon"],
-                "tue": routines_by_day["tue"],
-                "wed": routines_by_day["wed"],
-                "thu": routines_by_day["thu"],
-                "fri": routines_by_day["fri"],
-                "sat": routines_by_day["sat"],
-                "sun": routines_by_day["sun"],
-                "routines": routines_data
-            }
-            return make_response(status_code=200, data=data)
-        except Exception as e:
-            return make_response(550, data={'error': str(e)})
+        data = {
+            "mon": routines_by_day["mon"],
+            "tue": routines_by_day["tue"],
+            "wed": routines_by_day["wed"],
+            "thu": routines_by_day["thu"],
+            "fri": routines_by_day["fri"],
+            "sat": routines_by_day["sat"],
+            "sun": routines_by_day["sun"],
+            "routines": routines_data
+        }
+        return make_response(status_code=200, data=data)
