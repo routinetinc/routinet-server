241c908ced129e6aacf4a3546bb98bd7e2972d32
ozatamago <ozatamago40@gmail.com>
2023-10-08 18:00:24 +0900

    feed.TaskCompleteCommentを作成

--- feed/views.py
+++ feed/views.py
@@ -10,7 +10,8 @@ from django.utils import timezone
 from feed.user_actions import TaskFinish as taskfinish
 from .utils.graph_db.connections import neo4j_session
 from routine.utils.handle_json import *
-from .models import FeedPostComment as feedpostcomment, FeedPost as feedpost
+from .models import TaskFinishComment as taskfinishcomment
+from routine.models import TaskFinish as taskfinish
 from supply_auth.models import *
 
 
@@ -40,48 +41,25 @@ class Delete(APIView):
     
 
 
-class FeedPostCommentSerializer(serializers.Serializer):
-    post_id = serializers.IntegerField(max_value=None, min_value=None)
+class TaskCompleteCommentSerializer(serializers.Serializer):
+    task_finish_id = serializers.IntegerField(max_value=None, min_value=None)
     comment = serializers.CharField(max_length=400)
 
-class CommentUserSerializer(serializers.Serializer):
-    comment_id = serializers.IntegerField(source='id')
-    user_id = serializers.IntegerField(source='feed_post_id.user.id')
-    profile_media_id = serializers.IntegerField(source='feed_post_id.user.profile_media_id')
-    username = serializers.CharField(source='feed_post_id.user.username')
-    comment = serializers.CharField()
-    content_media_id = serializers.IntegerField(allow_null=True, required=False, source='media_id')
-    post_time = serializers.DateTimeField()
-    like_num = serializers.IntegerField()
-
-
-class FeedPostComment(APIView):
-    def get(self, request):
-        # Assume feed_post_id
-        feed_post_id = 1
-
-        # Retrieve FeedPostComment instances associated with the given feed_post_id
-        comments = feedpostcomment.objects.filter(feed_post_id=feed_post_id)
-
-        # Serialize the comments
-        serializer = CommentUserSerializer(comments, many=True)
-
-        return make_response(status_code=1, data={"comment_list": serializer.data})
-    
+class TaskCompleteComment(APIView):  
     def post(self, request, format=None):
-        data = get_json(request, FeedPostCommentSerializer)
+        data = get_json(request, TaskCompleteCommentSerializer)
 
         # Debug print
         print("Received data:", data)
 
-        feed_post = feedpost.objects.get(id=data['post_id'])            
+        task_finish = taskfinish.objects.get(id=data['task_finish_id'])            
 
         # Debug print
-        print("Fetched feed_post:", feed_post)
+        print("Fetched task_finish:", task_finish)
 
         # Save the comment
-        comment = feedpostcomment(
-            feed_post_id=feed_post, 
+        comment = taskfinishcomment(
+            task_finish_id=task_finish, 
             comment=data['comment'],
             post_time=timezone.now()  # Set the post_time to the current time
         )
@@ -91,4 +69,4 @@ class FeedPostComment(APIView):
         print("Created comment:", comment)
 
         # Return the comment ID in the response
-        return make_response(status_code=1, data={"content_id": comment.pk})
+        return make_response(status_code=1, data={"task_finish_id": comment.pk})
