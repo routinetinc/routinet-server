9236fc9f63d2333f8923594b79e279efd8ea0ee5
hukurou <junjun.0712.on@gmail.com>
2023-11-01 11:44:46 +0900

    ひとつ前のコミットのエラー解消

--- supply_auth/models.py
+++ supply_auth/models.py
@@ -65,8 +65,8 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     objects = UserManager()
 
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['email']
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
         
     def __str__(self):
         return self.username
