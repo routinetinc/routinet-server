0fc75d16311c98aba021a7095b293c21edc8b155
hukurou <junjun.0712.on@gmail.com>
2023-11-07 23:30:56 +0900

    apply changing db

--- feed/models.py
+++ feed/models.py
@@ -5,7 +5,8 @@ import boto3
 from django.contrib.postgres.fields import ArrayField
 from supply_auth.models import User  # 2つ目のmodels.pyからインポート
 from routine.fields import CustomModels  # 1つ目のmodels.pyから仮にインポート（必要に応じて）
-from routine.models import Interest, TaskFinish  # 1つ目のmodels.pyからインポート
+from routine.models import RoutineFinish  # 1つ目のmodels.pyからインポート
+from django.utils import timezone
 
 class NoSQLBase(models.Model):
     table_name = ''
@@ -46,48 +47,64 @@ class Cache():
         table_name = 'usercache'
 
 
-# Challengeモデルは独自に定義（適切な場所からインポートする必要があるかもしれません）
-class Challenge(models.Model):
-    name = models.CharField(max_length=255)
+class Interest(models.Model): # 外部キーのため依存解消のために仮置き
+    table_name  = 'interest'
+    name        = models.CharField(max_length=20) 
+    detail      = models.CharField(max_length=50) 
+    
+    def __str__(self):
+        return f"Interest name is 「{self.name}」"
+    
+class Tag(models.Model):
+    table_name  = 'tag'
+    name        = models.CharField(max_length=20) 
+    detail      = models.CharField(max_length=50) 
+
+    def __str__(self):
+        return f"Tag name is 「{self.name}」"
 
 class FeedPost(models.Model):
-    table_name = "feed_post"
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    sentence = models.CharField(max_length=400, blank=True)
-    media_id = models.IntegerField(null=True, blank=True)
-    post_time = models.DateTimeField()
-    like_num = models.IntegerField(default=0)
+    table_name   = "feed_post"
+    user_id      = models.ForeignKey(User, on_delete=models.CASCADE)
+    sentence     = models.CharField(max_length=400, blank=True)
+    #media_id    = models.IntegerField(null=True, blank=True)
+    post_time    = models.DateTimeField()
+    like_num     = models.IntegerField(default=0)
     interest_ids = ArrayField(models.IntegerField(null=True, blank=True))
-    challenge_id = models.ForeignKey(Challenge, on_delete=models.SET_NULL, null=True, blank=True)
+    tag_id       = models.ForeignKey(Tag, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
-        return self.sentence
-
-
-    def __str__(self):
-        return self.sentence
+        return f"Sentence is 「{self.sentence}」"
+    
+    def save(self, *args, **kwargs):
+        self.when = timezone.now()  # 保存されるたびに更新
+        return super(FeedPost, self).save(*args, **kwargs)
 
 class FeedPostComment(models.Model):
     feed_post_id = models.ForeignKey(FeedPost, on_delete=models.CASCADE)
-    post_time = models.DateTimeField()
-    media_id = models.IntegerField(null=True, blank=True)
-    like_num = models.IntegerField(default=0)
+    post_time    = models.DateTimeField()
+    #media_id    = models.IntegerField(null=True, blank=True)
+    like_num     = models.IntegerField(default=0)
+    comment      = models.CharField(max_length=400, blank=True)
 
     def __str__(self):
-        return f"Comment by {self.feed_post.user} on {self.post_time}"
+        return f"Comment on FeedPost {self.feed_post_id} at {self.post_time}"
+    
+    def save(self, *args, **kwargs):
+        self.when = timezone.now()  # 保存されるたびに更新
+        return super(FeedPostComment, self).save(*args, **kwargs)
     
 
-class TaskFinishComment(models.Model):
-    task_finish_id = models.ForeignKey(TaskFinish, on_delete=models.CASCADE)
-    post_time = models.DateTimeField()
-    media_id = models.IntegerField(null=True, blank=True)
-    like_num = models.IntegerField(default=0)
+class RoutineFinishComment(models.Model):
+    routine_finish_id = models.ForeignKey(RoutineFinish, on_delete=models.CASCADE)
+    post_time         = models.DateTimeField()
+    #media_id         = models.IntegerField(null=True, blank=True)
+    like_num          = models.IntegerField(default=0)
+    comment           = models.CharField(max_length=400, blank=True)
 
     def __str__(self):
-        return f"Comment on TaskFinish {self.task_finish_id} at {self.post_time}"
-
-class Tag(models.Model):
-    name = models.CharField(max_length=400, blank=True)
-
-    def __str__(self):
-        return f"{self.name}"
+        return f"Comment on Routine_finish {self.routine_finish_id} at {self.post_time}"
+    
+    def save(self, *args, **kwargs):
+        self.when = timezone.now()  # 保存されるたびに更新
+        return super(FeedPostComment, self).save(*args, **kwargs)
