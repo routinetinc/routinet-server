a4ff6efbe9cce83894f6ef56a472ddea3606cb6d
ozatamago <ozatamago40@gmail.com>
2023-11-11 15:24:07 +0900

    routine_taskを修正

--- routine/views_package/routine.py
+++ routine/views_package/routine.py
@@ -1,9 +1,13 @@
 from rest_framework.views import APIView
 from datetime import datetime, timedelta
 from routine import models, serializers
+# from routine.models import Routine, RoutineFinish, Task, TaskFinish
 from routine.utils.handle_json import RequestInvalid, get_json, make_response
 from routine import serializers
 from supply_auth.models import User as UserModel
+from django.http import JsonResponse, HttpRequest
+from datetime import datetime, timedelta
+from django.utils.dateparse import parse_date
 
 class Routine(APIView):
     def get(self, request, format=None):
@@ -70,42 +74,45 @@ class Routine(APIView):
 
 
 #ログインユーザーの、一週間分のルーティーンとタスクを取得する。
-class ReadRoutineAndTask(APIView):                 
+class ReadRoutineAndTask(APIView):
     def get(self, request, format=None):
-        user_id = 1 if(request.user.id is None) else request.user.id
+        user_id = 1 if request.user.id is None else request.user.id
         user = UserModel.objects.get(id=user_id)
+        print("1")
 
-        #dowのリストを、[0, 1, 2, 3, 4, 5, 6]の形式で、その日から順番に用意
-        now = datetime.now()
-        dow_list = [(now + timedelta(days=i)).weekday() for i in range(7)]
+        # Parse the week_start query parameter to a date object
+        week_start_str = request.query_params.get('week_start')
+        if week_start_str and len(week_start_str) == 8:  # Check if the parameter is present and of the correct length
+            week_start_formatted_str = f"{week_start_str[:4]}-{week_start_str[4:6]}-{week_start_str[6:]}"
+            week_start_date = parse_date(week_start_formatted_str)
+            print(week_start_date)
+        else:
+            # Handle the case where week_start is not provided or not properly formatted
+            return make_response(status_code=400, data={'message': 'Invalid week_start parameter'})
+
+        # Calculate the start (Monday) and end (Sunday) of the week
+        start_of_week = week_start_date - timedelta(days=week_start_date.weekday())
+        print(start_of_week)
+
+        end_of_week = start_of_week + timedelta(days=6)
+        print("1")
 
         routines_data = {}
-        routines_by_day = {
-            "mon": [],
-            "tue": [],
-            "wed": [],
-            "thu": [],
-            "fri": [],
-            "sat": [],
-            "sun": []
-        }
-        day_of_week = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+        routines_by_day = {day: [] for day in ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']}
+        print("1")
 
-        for dow in dow_list:
-            #SQL文の実行結果をDjangoのインスタンスとして返す
-            routines = models.Routine.objects.raw(
-            
-                #models.Routine.objects.raw(): Djangoで生のSQLクエリを実行。
-                # SELECT * FROM routine_routine: ルーティンテーブルから全カラムを取得。
-                # WHERE user_id_id = %s: ユーザーIDに基づくレコードのフィルタリング。
-                # AND (dow & %s) != 0: 曜日情報（dow）をビット単位で解析し、該当するレコードをフィルタリング。
-                # ORDER BY start_time: 結果をルーティンの開始時間順にソート。
+        # Iterate over each day of the week starting from Monday
+        for i in range(7):
+            day_date = start_of_week + timedelta(days=i)
+            day_code = day_date.weekday()
+            day_name = day_date.strftime('%a').lower()
+            print(day_date, day_code, day_name)
 
-                "SELECT * FROM routine_routine WHERE user_id_id = %s AND (dow & %s) != 0 ORDER BY start_time",
-
-                # [user.id, 2**dow]: フィルタリングと解析のためのパラメータを渡す。
-                [user.id, 2**dow])
-
+            routines = models.Routine.objects.filter(
+                user_id=user_id,
+                dow=day_code  # Assuming 'dow' can be compared directly to 'day_code'. Adjust if needed.
+            ).order_by('start_time')
+            print("1")
             for routine in routines:
                 if str(routine.id) not in routines_data:
                     tasks = models.Task.objects.filter(routine_id=routine).order_by('id')
@@ -118,21 +125,23 @@ class ReadRoutineAndTask(APIView):
                             "title": task.title,
                             "detail": task.detail,
                             "required_time": task.required_time,
-                            "is_notified": task.is_notified,
                             "is_achieved": is_achieved,
+                            # "tag_id": routine.tag_id.id if routine.tag_id else None,  # Assuming `tag_id` is a foreign key to another model
+                            # "real_time": routine.is_real_time,  # Assuming `is_real_time` corresponds to `real_time`
+                            # "tag_name": routine.tag_id.name if routine.tag_id else '',  # Replace with actual field name for the tag's name
+                            # "consecutive_days": routine.calculate_consecutive_days(),  # Call the method on the routine instance
                         })
                     routines_data[str(routine.id)] = {
                         "start_time": routine.start_time,
                         "end_time": routine.end_time,
                         "title": routine.title,
-                        "subtitle": routine.subtitle,
-                        "is_published": routine.is_published,
-                        "is_notified": routine.is_notified,
+                        "tag_id": routine.tag_id.id if routine.tag_id else None,
                         "tasks": task_data
                     }
-                routines_by_day[day_of_week[dow]].append(str(routine.id))
+                routines_by_day[day_name].append(str(routine.id))
+        print("1")
 
-
+        # Build the data structure for the response
         data = {
             "mon": routines_by_day["mon"],
             "tue": routines_by_day["tue"],
@@ -143,4 +152,4 @@ class ReadRoutineAndTask(APIView):
             "sun": routines_by_day["sun"],
             "routines": routines_data
         }
-        return make_response(status_code=200, data=data)
+        return make_response(status_code=1, data=data)
